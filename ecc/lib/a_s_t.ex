119281eae87b3e790876e2ad81eb5e074fa77378
Ernesto Aguilera <leapvader_1998@hotmail.com>
2019-09-15 19:10:37 -0500

    Removidos archivos del IDE y agregados el generador de c´odigo y el parser

--- ecc/lib/a_s_t.ex
+++ ecc/lib/a_s_t.ex
@@ -1,4 +1,8 @@
 defmodule AST do
   @moduledoc false
+<<<<<<< HEAD
   defstruct [:nodeID, :nodeValue, :leftNode, :rightNode]
+=======
+  defstruct [:id, :value, :leftNode, :rightNode]
+>>>>>>> Removidos archivos del IDE y agregados el generador de c´odigo y el parser
 end
--- ecc/lib/limp.ex
+++ ecc/lib/limp.ex
@@ -1,6 +1,10 @@
 defmodule Limpiador do
 
+<<<<<<< HEAD
   def limpiaEspacios(source) do
+=======
+  def limpiaEspacios(source) do #Quita los espacios en blanco incluyendo saltos de linea y tabuladores
+>>>>>>> Removidos archivos del IDE y agregados el generador de c´odigo y el parser
     limpio = String.trim(source)
     Regex.split(~r/\s+/, source)
   end
